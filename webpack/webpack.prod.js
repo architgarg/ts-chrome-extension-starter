1cf113a5c754d757eae750753b2cfb90bbea5942
Archit Garg <thearchitgargg@gmail.com>
2020-07-12 16:59:15 +0530

    Added terser plugin for minification of bundles

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,21 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common(false), {
   mode: "production",
   optimization: {
-    minimizer: [new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin()]
-  }
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+          output: {
+            ascii_only: true
+          },
+        },
+      })
+    ],
+  },
 });
